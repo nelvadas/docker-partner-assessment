781a471cb7a2778ed972947932578bd8ddfee148
Eric Collazo <ericcollazo67@gmail.com>
2019-06-14 10:59:54 -0400

    Removed unneeded files.

--- exercise-3/ra-db/DeleteDB.ps1
+++
@@ -1,8 +0,0 @@
-# Connect to Azure services (only needs to be used once per Powershell session)
-# Connect-AzAccount
-
-# The resource name for your resources
-$resourcegroupname = "ra-db-rg"
-
-# Remove resource group and it's contents
-Remove-AzResourceGroup -ResourceGroupName $resourcegroupname
